e2969d84ab577a0c5e75aeee9f31a6fb9b68f0f8
Ruoyu Sun <ruoysun@gmail.com>
2018-02-17 14:08:43 +0200

    New version

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ import {
   TouchableNativeFeedback,
   NativeModules,
   Alert,
+  Dimensions,
   Image,
   ToastAndroid,
   ActivityIndicator,
@@ -49,7 +50,19 @@ export default class tubefm extends Component {
   }
 
   _onVideoIdAdded = (e) => {
-    this.download(e.videoId);
+    this.getVideoInfo(e.videoId);
+  }
+
+  getVideoInfo = (videoId) => {
+    this.setState({ loading: true });
+    getInfo(`https://www.youtube.com/watch?v=${videoId}`, (error, info) => {
+      if (error) {
+        alert(error);
+        this.setState({ loading: false });
+        return;
+      }
+      this.setState({ info: info, loading: false });
+    });
   }
 
   _renderVideoInfo = () => {
@@ -57,21 +70,62 @@ export default class tubefm extends Component {
       <ActivityIndicator size="large" style={{ marginTop: 20 }} />
     );
     if (!this.state.info) return null;
+    const { info } = this.state;
+    var { height, width } = Dimensions.get('window')
     return (
-      <FlatList
-        style={{ flexGrow: 1 }}
-        keyExtractor={(i) => i.itag}
-        data={this.state.info.formats}
-        renderItem={this._renderRow}
-      />
+      <View style={{ flex: 1 }}>
+        <View>
+          <Image
+            resizeMode="cover"
+            source={{ uri: info.thumbnail_url.replace('default.jpg', 'hqdefault.jpg') }}
+            style={{ height: width * 9 / 16, width: width }}
+          />
+          <View style={{
+            paddingHorizontal: 20,
+            backgroundColor: 'rgba(0,0,0,0.6)',
+            height: 50,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            <Text numberOfLines={1} style={{ fontSize: 20, color: '#fff' }}>{info.title}</Text>
+          </View>
+        </View>
+        <FlatList
+          style={{ flex: 1 }}
+          keyExtractor={(i) => i.itag}
+          data={info.formats}
+          renderItem={this._renderRow}
+        />
+      </View >
     );
   }
 
-  _renderRow = ({ item }) => (
-    <View>
-      <Text>{item.resolution} {item.container}</Text>
-    </View>
-  )
+  _renderRow = ({ item }) => {
+    const { info } = this.state;
+    return (
+      <TouchableNativeFeedback onPress={() => {
+        const fileName = `${info.title}.${item.container}`;
+        NativeHelpers.download(item.url, fileName);
+        ToastAndroid.show(`Start to download ${fileName}`, ToastAndroid.SHORT)
+      }}>
+        <View style={{
+          height: 50,
+          paddingHorizontal: 20,
+          borderTopWidth: StyleSheet.hairlineWidth,
+          borderTopColor: '#eee',
+          flexDirection: 'row',
+          alignItems: 'center'
+        }}>
+          <Text>{item.resolution || 'Audio'} ({item.encoding || item.audioEncoding}) </Text>
+          <Text style={{ flexGrow: 1, textAlign: 'right', fontWeight: 'bold' }}>{item.container.toUpperCase()}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    )
+  }
 
   render() {
     return (
@@ -96,33 +150,18 @@ export default class tubefm extends Component {
         </TouchableNativeFeedback>
         <View style={{ backgroundColor: '#ccc', height: StyleSheet.hairlineWidth }} />
         <View style={{ backgroundColor: '#eee', height: StyleSheet.hairlineWidth * 2 }} />
-        <View style={{ backgroundColor: '#f1f1f1', paddingHorizontal: 10, borderBottomColor: '#ddd', borderBottomWidth: StyleSheet.hairlineWidth }}>
+        <View style={{ backgroundColor: '#f4f4f4', paddingHorizontal: 10, borderBottomColor: '#ddd', borderBottomWidth: StyleSheet.hairlineWidth }}>
           <TextInput
-            defaultValue="qr1Fpkkad2c"
             returnKeyType="done"
             underlineColorAndroid="transparent"
             style={{ flexGrow: 1, fontSize: 24, color: '#666' }}
             placeholder="youtube.com/watch?v="
-            onSubmitEditing={(e) => this.download(e.nativeEvent.text)} />
+            onSubmitEditing={(e) => this.getVideoInfo(e.nativeEvent.text)} />
         </View>
         {this._renderVideoInfo()}
-      </View>
+      </View >
     );
   }
-
-  download(videoId) {
-    this.setState({ loading: true });
-    getInfo(`https://www.youtube.com/watch?v=${videoId}`, (error, info) => {
-      if (error) {
-        alert(error);
-        this.setState({ loading: false });
-        return;
-      }
-      console.log(info.formats);
-      this.setState({ info: info, loading: false });
-    });
-  }
-
 }
 
 const styles = StyleSheet.create({
